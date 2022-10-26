ca2b134b679b5310bbd943d5c9b17aa72845a763
NoahSimon8 <noah.simon9806@gmail.com>
2022-10-25 22:45:09 -0700

    ShooterSubSystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,13 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+
+
+    public static final class Shooter{
+        public static final int right_shooter_motor = 15;
+        public static final int left_shooter_motor = 16;
+    
+    }
+}
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,11 +1,105 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import frc.robot.Constants;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 public class Shooter extends SubsystemBase {
+
+
+  private MotorController leftShooterMotor = new MotorController(){
+
+    @Override
+    public void set(double speed) {
+      // TODO Auto-generated method stub
+    }
+
+    @Override
+    public double get() {
+      // TODO Auto-generated method stub
+      return 0;
+    }
+
+    @Override
+    public void setInverted(boolean isInverted) {
+      // TODO Auto-generated method stub
+      
+    }
+
+    @Override
+    public boolean getInverted() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+    @Override
+    public void disable() {
+      // TODO Auto-generated method stub
+      
+    }
+
+    @Override
+    public void stopMotor() {
+      // TODO Auto-generated method stub
+      
+    }
+
+  };
+    
+  
+  private MotorController rightShooterMotor = new MotorController(){
+
+    @Override
+    public void set(double speed) {
+      // TODO Auto-generated method stub
+      
+    }
+
+    @Override
+    public double get() {
+      // TODO Auto-generated method stub
+      return 0;
+    }
+
+    @Override
+    public void setInverted(boolean isInverted) {
+      // TODO Auto-generated method stub
+      
+    }
+
+    @Override
+    public boolean getInverted() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+    @Override
+    public void disable() {
+      // TODO Auto-generated method stub
+      
+    }
+
+    @Override
+    public void stopMotor() {
+      // TODO Auto-generated method stub
+      
+    }
+  };
+
+
+  private MotorControllerGroup shooter = new MotorControllerGroup(leftShooterMotor,rightShooterMotor );
+
+
+
   /** Creates a new ExampleSubsystem. */
-  public Shooter() {}
+  public Shooter() {
+
+    int test = Constants.Shooter.left_shooter_motor;
+
+    
+  }
 
   @Override
   public void periodic() {
